a0a8f60a33f42ad3ef8ec15c1aec1a5a998c5bc4
Vinal Patel <vinalpatel18@gmail.com>
2019-05-15 15:22:57 -0400

    updates

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const hbs = require('hbs')
-const port = process.env.PORT || 3000
+const ports = process.env.PORT || 3000
 
 
 //app.com
@@ -76,6 +76,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(port, () => {
+app.listen(ports, () => {
     console.log('Server is up and runnig')
 })
